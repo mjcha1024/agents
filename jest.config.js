bbea7cc8e7f94a067e68cfc3d43948db0038d472
jcha-ultra <john@ultratesting.us>
2021-02-14 06:20:51 -0500

    Add basic Jest test

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/en/configuration.html
+ */
+
+module.exports = {
+    testEnvironment: 'node',
+    slowTestThreshold: 120000, // Two minutes
+    testTimeout: 300000, // Five minutes
+};
---
+++ tests/basic.test.js
@@ -0,0 +1,4 @@
+test('basic jest test', () => {
+
+    expect(1).toBe(1);
+});
